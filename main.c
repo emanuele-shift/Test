dfd01a48cbfa4b385c06ce848cb859b06e7374b4
emanuele-shift <157368956+emanuele-shift@users.noreply.github.com>
2024-01-24 08:40:56 +0100

    fighissimo commento
    aggiornamento

--- main.c
+++ main.c
@@ -3,32 +3,55 @@
 #include <time.h>
 
 int main() {
+
+    //random number seed
+
     srand(time(0));
     int n, min, max, random, i = 1;
-    int contatore = 0;
+
+    // how many numbers I need to control if they're prime or not
+
     do {
         printf("numero numeri:");
         scanf("%d", &n);
     } while (n <= 0);
+
+    //minumum number
+
     printf("numero minimo:");
     scanf("%d", &min);
+
+    //maximum number
 
     do {
         printf("numero massimo:");
         scanf("%d", &max);
     } while (max < min);
+
+    //generate a random number
+
     for (i; i < n + 1; i++) {
         random = (rand() % (max - min + 1) + min);
         printf("**il numero numero %d, e' %d\n", i, random);
-        int j = 1;
+        int j = 2;
+        int contatore = 0;
+
+        //control if the number is prime or not
+
         for (j; j < random; j++) {
+
+            //number not prime
+
             if (random % j == 0) {
                 contatore = contatore + 1;
                 printf("%d e' divisore del numero %d e' indiano\n", j, random);
             }
-            if (contatore == 1) {
-                printf("il numero e' primo, dato che e' dal burkina faso\n");
-            }
+        }
+
+        //prime number
+
+        if (contatore == 0) {
+            printf("il numero e' primo, dato che e' dal burkina faso\n");
         }
     }
     return 0;
